4fa4e217da275f5a1886f974e64b7c3b1d7cc080
Murilo-Gaspareto <murigaspareto@gmail.com>
2025-04-07 14:53:37 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,51 @@
-// Seleciona todos os botões de personagem e os elementos de personagens
+// Seleciona todos os botões de personagem
 const botoes = document.querySelectorAll(".botao");
+// Seleciona todos os personagens
 const personagens = document.querySelectorAll(".personagem");
 
-// Inicializa os eventos de clique nos botões
-function inicializarEventosDosBotoes() {
-    botoes.forEach((botao, indice) => {
-        botao.addEventListener("click", () => {
-            desmarcarBotaoSelecionado();
-            marcarBotaoSelecionado(botao);
+// Adiciona o evento de clique em cada botão
+botoes.forEach((botao, indice) => {
+	botao.addEventListener("click", () => {
+		removerSelecaoBotao();
+		selecionarBotao(botao);
 
-            esconderPersonagemSelecionado();
-            mostrarPersonagemSelecionado(indice);
-        });
-    });
+		removerSelecaoPersonagem();
+		selecionarPersonagem(indice);
+	});
+});
+
+/**
+ * Remove a classe "selecionado" do botão que está atualmente selecionado
+ */
+function removerSelecaoBotao() {
+	const botaoSelecionado = document.querySelector(".botao.selecionado");
+	if (botaoSelecionado) {
+		botaoSelecionado.classList.remove("selecionado");
+	}
 }
 
-// Remove a classe 'selecionado' do botão atualmente selecionado, se houver
-function desmarcarBotaoSelecionado() {
-    const botaoSelecionado = document.querySelector(".botao.selecionado");
-    if (botaoSelecionado) {
-        botaoSelecionado.classList.remove("selecionado");
-    }
+/**
+ * Adiciona a classe "selecionado" ao botão clicado
+ * @param {Element} botao - Botão que foi clicado
+ */
+function selecionarBotao(botao) {
+	botao.classList.add("selecionado");
 }
 
-// Adiciona a classe 'selecionado' ao botão clicado
-function marcarBotaoSelecionado(botao) {
-    botao.classList.add("selecionado");
+/**
+ * Remove a classe "selecionado" do personagem atualmente exibido
+ */
+function removerSelecaoPersonagem() {
+	const personagemSelecionado = document.querySelector(".personagem.selecionado");
+	if (personagemSelecionado) {
+		personagemSelecionado.classList.remove("selecionado");
+	}
 }
 
-// Remove a classe 'selecionado' do personagem atualmente exibido, se houver
-function esconderPersonagemSelecionado() {
-    const personagemSelecionado = document.querySelector(".personagem.selecionado");
-    if (personagemSelecionado) {
-        personagemSelecionado.classList.remove("selecionado");
-    }
+/**
+ * Adiciona a classe "selecionado" ao personagem correspondente ao botão clicado
+ * @param {number} indice - Índice do botão clicado, que corresponde ao personagem
+ */
+function selecionarPersonagem(indice) {
+	personagens[indice].classList.add("selecionado");
 }
-
-// Adiciona a classe 'selecionado' ao personagem correspondente ao botão clicado
-function mostrarPersonagemSelecionado(indice) {
-    personagens[indice].classList.add("selecionado");
-}
-
-// Inicia o script
-inicializarEventosDosBotoes();
